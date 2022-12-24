73d999dbe435caf74c32ac305996feaf24564b95
zhenghaoz <zhangzhenghao@hotmail.com>
2022-12-25 03:16:48 +0800

    implement async methods (#4)

--- Gorse.NET.Tests/GorseTest.cs
+++ Gorse.NET.Tests/GorseTest.cs
@@ -39,6 +39,30 @@ public class Tests
     }
 
     [Test]
+    public async Task TestUsersAsync()
+    {
+        // Insert a user
+        var user = new User { UserId = "1", Labels = new string[] { "a", "b", "c" } };
+        var rowAffected = await client.InsertUserAsync(user);
+        Assert.That(rowAffected.RowAffected, Is.EqualTo(1));
+        // Get this user.
+        var returnUser = await client.GetUserAsync("1");
+        Assert.That(returnUser, Is.EqualTo(user));
+        // Delete this user.
+        rowAffected = await client.DeleteUserAsync("1");
+        Assert.That(rowAffected.RowAffected, Is.EqualTo(1));
+        try
+        {
+            returnUser = await client.GetUserAsync("1");
+            Assert.Fail();
+        }
+        catch (GorseException e)
+        {
+            Assert.That(e.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+    }
+
+    [Test]
     public void TestFeedback()
     {
         // Insert feedback
@@ -48,6 +72,19 @@ public class Tests
             new Feedback{FeedbackType="read", UserId="10", ItemId="4", Timestamp="2022-11-20T13:55:27Z"},
         };
         var rowAffected = client.InsertFeedback(feedbacks);
+        Assert.That(rowAffected.RowAffected, Is.EqualTo(2));
+    }
+
+    [Test]
+    public async Task TestFeedbackAsync()
+    {
+        // Insert feedback
+        var feedbacks = new Feedback[]
+        {
+            new Feedback{FeedbackType="read", UserId="10", ItemId="3", Timestamp="2022-11-20T13:55:27Z"},
+            new Feedback{FeedbackType="read", UserId="10", ItemId="4", Timestamp="2022-11-20T13:55:27Z"},
+        };
+        var rowAffected = await client.InsertFeedbackAsync(feedbacks);
         Assert.That(rowAffected.RowAffected, Is.EqualTo(2));
     }
 
@@ -62,6 +99,20 @@ public class Tests
             new SortedSetEntry("30", 3)
         });
         var items = client.GetRecommend("10");
+        Assert.That(items, Is.EqualTo(new string[] { "30", "20", "10" }));
+    }
+
+    [Test]
+    public async Task TestRecommendAsync()
+    {
+        var db = redis.GetDatabase();
+        await db.SortedSetAddAsync("offline_recommend/10", new SortedSetEntry[]
+        {
+            new SortedSetEntry("10", 1),
+            new SortedSetEntry("20", 2),
+            new SortedSetEntry("30", 3)
+        });
+        var items = await client.GetRecommendAsync("10");
         Assert.That(items, Is.EqualTo(new string[] { "30", "20", "10" }));
     }
 }
